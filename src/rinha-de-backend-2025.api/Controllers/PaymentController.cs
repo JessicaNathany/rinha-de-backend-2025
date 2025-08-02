f8e04b8bcbfcb4cb8c8d205b5020d290da6579bf
Jéssica <jessicanathany.web@gmail.com>
2025-08-02 17:52:13 -0300

    adicionando projeto da rinha

---
+++ src/rinha-de-backend-2025.api/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rinha_de_backend_2025.api.Request;
+
+namespace rinha_de_backend_2025.api.Controllers
+{
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost]
+        [Route("payment")]
+        public async Task<IActionResult> Payment(PaymentRequest request)
+        {
+            return Created();
+
+        }
+
+        [HttpGet]
+        [Route("payment-summary")]
+        public async Task<IActionResult> PaymentSummary()
+        {
+            return Ok();
+
+        }
+    }
+}
---
+++ src/rinha-de-backend-2025.api/Request/PaymentRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace rinha_de_backend_2025.api.Request
+{
+    public class PaymentRequest
+    {
+        public string CorrelationId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
